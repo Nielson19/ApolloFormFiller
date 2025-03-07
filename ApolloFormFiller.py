00c87f947c56258dcec91d2e0822542e0d57b228
mijunior325 <151282962+mijunior325@users.noreply.github.com>
2025-03-07 18:08:30 -0400

    Nuevo PDF
    Se agrega el  nuevo pdf y tambien se deja en disable

--- ApolloFormFiller.py
+++ ApolloFormFiller.py
@@ -32,6 +32,7 @@ def main():
         "Tarp Contract 2025": get_resource_path("Templates/TARP APOLLO 2025.pdf"),
         "WM Contract 2025": get_resource_path("Templates/WM APOLLO 2025.pdf"),
         "Mold Contract 2025": get_resource_path("Templates/MOLD APOLLO 2025.pdf"),
+        "FMS signed contract LOP": get_resource_path("Templates/FMS signed contract LOP.pdf"),
     }
 
     # Configurar el modo de apariencia y el tema
@@ -99,7 +100,8 @@ def main():
         if labelName in ["Estimated Total for Flood Contract 2025", 
                      "Estimated Total for Tarp Contract 2025", 
                      "Estimated Total for WM Contract 2025", 
-                     "Estimated Total for Mold Contract 2025"]:
+                     "Estimated Total for Mold Contract 2025", 
+                     "Estimated Total for FMS signed contract LOP"]:
             inputSpace.configure(state='disabled')
 
         return value
